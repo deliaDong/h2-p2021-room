57ef6a065babf8c332a2dd3e59b81fb1ad792316
lihbr <lihbr@users.noreply.github.com>
2018-05-02 21:03:54 +0200

    :book: Utils3 v1.0

--- dev/assets/js/Room.js
+++ dev/assets/js/Room.js
@@ -19,16 +19,24 @@ class Room {
   createContext ($output) {
     this._ctx = {}
 
+    // DOM
     this._ctx.$output = $output
     this._ctx.$canvas = $output.querySelector("canvas")
 
+    // GLOBAL
     this._ctx.w = this._ctx.$output.offsetWidth
     this._ctx.h = this._ctx.$output.offsetHeight
 
+    // VIEW
     this._ctx.cAngle = 70 // Camera angle
-
     this._ctx.scene = new THREE.Scene()
-    this._ctx.camera = new Utils3.Camera(this._ctx.scene, this._ctx.cAngle, this._ctx.w, this._ctx.h)
+    this._ctx.camera = new Utils3.Camera(
+      this._ctx.scene,
+      this._ctx.cAngle,
+      this._ctx.w,
+      this._ctx.h,
+      3
+    )
     this._ctx.renderer = new Utils3.Renderer({
       canvas: this._ctx.$canvas,
       width: this._ctx.w,
@@ -39,13 +47,36 @@ class Room {
     this._ctx.composer = new Utils3.Composer({
       renderer: this._ctx.renderer,
       scene: this._ctx.scene,
-      camera: this._ctx.camera
+      camera: this._ctx.camera,
+      bloom: true,
+      film: true,
+      bleach: true,
+      vignette: true
     })
+
+    // CTRL
+    this._ctx.cameraYOffset = 0 // Y angle offset
+    this._ctx.speed = 0.05
+    this._ctx.mouse = {x: 0, y: 0}
+    this._ctx.input = {}
+    this._ctx.keyboard = {
+      qwerty: "wasd",
+      azerty: "zqsd"
+    }
+    this._ctx.keyBoardType = "azerty"
   }
   
   // Init everything
   init () {
     window.addEventListener("resize", this.sizeUpdate.bind(this))
+    // Update mouse
+    this._ctx.$output.addEventListener("mousemove", (e) => {
+      this._ctx.mouse.x = Math.round((e.clientX / this._ctx.w - 0.5) * 100) / 100
+      this._ctx.mouse.y = Math.round((e.clientY / this._ctx.h - 0.5) * 100) / -100
+    })
+    // Update keyboard
+    document.addEventListener("keydown", (e) => { this._ctx.input[e.key] = true })
+    document.addEventListener("keyup", (e) => { this._ctx.input[e.key] = false })
     
     this.torus()
     this.loop()
@@ -56,6 +87,7 @@ class Room {
       new THREE.TorusKnotGeometry(.7, .2, 100, 8),
       new THREE.MeshPhongMaterial({ color: 0xff0000, flatShading: true})
     )
+    this._mesh.position.y = 1.2
     this._ctx.scene.add(this._mesh)
     
     this._floor = new THREE.Mesh(
@@ -63,7 +95,6 @@ class Room {
       new THREE.MeshPhongMaterial({ color: 0x00ff00})
     )
     this._floor.rotation.x = -Math.PI * 0.5
-    this._floor.position.y = -1.2
     this._ctx.scene.add(this._floor)
     
     const ambientLight = new THREE.AmbientLight(0x111111)
@@ -86,9 +117,93 @@ class Room {
     this._ctx.composer.updateSize(this._ctx.w, this._ctx.h)
   }
   
+  // Main loop
   loop () {
     window.requestAnimationFrame(this.loop.bind(this))
+    this.updateCamera()
     this._mesh.rotation.y += 0.01
     this._ctx.composer.render()
+  }
+
+  // Update camera according to context
+  updateCamera () {
+
+    // Camera first person view
+    this._ctx.camera.set(
+      "angle",
+      {
+        x: Math.round(this._ctx.mouse.y * Math.PI / 2 * 1000) / 1000,
+        y: 0,
+        z: 0
+      }
+    )
+    
+    this._ctx.camera.set(
+      "angle",
+      {
+        x: 0,
+        y: Math.round((this._ctx.cameraYOffset - this._ctx.mouse.x * Math.PI / 2) * 1000) / 1000,
+        z: 0
+      },
+      true
+    )
+
+    if (this._ctx.mouse.x > 0.45) {
+      this._ctx.cameraYOffset -= Math.PI / 90
+    }
+
+    if (this._ctx.mouse.x < -0.45) {
+      this._ctx.cameraYOffset += Math.PI / 90
+    }
+
+    if (this._ctx.input[this._ctx.keyboard[this._ctx.keyBoardType][1]]) { // if left
+      this._ctx.camera.add(
+        "pos",
+        {
+          x: -Math.sin(this._ctx.camera.get("angle", true).y + Math.PI / 2) * this._ctx.speed,
+          y: 0,
+          z: -Math.cos(this._ctx.camera.get("angle", true).y + Math.PI / 2) * this._ctx.speed
+        },
+        true
+      )
+    }
+
+    if (this._ctx.input[this._ctx.keyboard[this._ctx.keyBoardType][3]]) { // if right
+      this._ctx.camera.add(
+        "pos",
+        {
+          x: Math.sin(this._ctx.camera.get("angle", true).y + Math.PI / 2) * this._ctx.speed,
+          y: 0,
+          z: Math.cos(this._ctx.camera.get("angle", true).y + Math.PI / 2) * this._ctx.speed
+        },
+        true
+      )
+    }
+
+    if (this._ctx.input[this._ctx.keyboard[this._ctx.keyBoardType][0]]) { // if up
+      this._ctx.camera.add(
+        "pos",
+        {
+          x: -Math.sin(this._ctx.camera.get("angle", true).y) * this._ctx.speed,
+          y: 0,
+          z: -Math.cos(this._ctx.camera.get("angle", true).y) * this._ctx.speed
+        },
+        true
+      )
+    }
+
+    if (this._ctx.input[this._ctx.keyboard[this._ctx.keyBoardType][2]]) { // if down
+      this._ctx.camera.add(
+        "pos",
+        {
+          x: Math.sin(this._ctx.camera.get("angle", true).y) * this._ctx.speed,
+          y: 0,
+          z: Math.cos(this._ctx.camera.get("angle", true).y) * this._ctx.speed
+        },
+        true
+      )
+    }
+
+
   }
 }
--- dev/assets/js/Utils3.js
+++ dev/assets/js/Utils3.js
@@ -6,12 +6,15 @@ const Utils3 = (function () {
    * Camera class
    */
   u.Camera = class {
-    constructor (scene, angle, width, height) {
+    constructor (scene, angle, width, height, cameraHeight = 0) {
       this._camera = new THREE.PerspectiveCamera(angle, width / height)
-      this._camera.position.z = 3
       this._camera.lookAt(new THREE.Vector3())
 
-      scene.add(this._camera)
+      this._camera.position.y = cameraHeight
+
+      this._holder = new THREE.Object3D()
+      this._holder.add(this._camera)
+      scene.add(this._holder)
     }
 
     updateSize (width, height) {
@@ -19,9 +22,84 @@ const Utils3 = (function () {
       this._camera.updateProjectionMatrix()
     }
 
-    get () {
-      return this._camera
+    get (param, holder = false) {
+      if (param == "angle") {
+        return holder ? this._holder.rotation : this._camera.rotation
+      } else if (param == "pos") {
+        return holder ? this._holder.position : this._camera.position
+      } else {
+        return holder ? this._holder : this._camera
+      }
     }
+
+    set (param, value, holder = false) {
+      if (value && param) {
+        if (param == "angle") {
+          if (holder) {
+            this._holder.rotation.x = value.x
+            this._holder.rotation.y = value.y
+            this._holder.rotation.z = value.z
+            return this._holder.rotation
+          } else {
+            this._camera.rotation.x = value.x
+            this._camera.rotation.y = value.y
+            this._camera.rotation.z = value.z
+            return this._camera.rotation
+          }
+        } else if (param == "pos") {
+          if (holder) {
+            this._holder.position.x = value.x
+            this._holder.position.y = value.y
+            this._holder.position.z = value.z
+            return this._holder.position
+          } else {
+            this._camera.position.x = value.x
+            this._camera.position.y = value.y
+            this._camera.position.z = value.z
+            return this._camera.position
+          }
+        } else {
+          console.error("Camera.set used with wrong param name (angle|pos)")
+        }
+      } else {
+        console.error("Camera.set used with wrong params")
+      }
+    }
+
+    add (param, value, holder = false) {
+      if (value && param) {
+        if (param == "angle") {
+          if (holder) {
+            this._holder.rotation.x += value.x
+            this._holder.rotation.y += value.y
+            this._holder.rotation.z += value.z
+            return this._holder.rotation
+          } else {
+            this._camera.rotation.x += value.x
+            this._camera.rotation.y += value.y
+            this._camera.rotation.z += value.z
+            return this._camera.rotation
+          }
+        } else if (param == "pos") {
+          if (holder) {
+            this._holder.position.x += value.x
+            this._holder.position.y += value.y
+            this._holder.position.z += value.z
+            return this._holder.position
+          } else {
+            this._camera.position.x += value.x
+            this._camera.position.y += value.y
+            this._camera.position.z += value.z
+            return this._camera.position
+          }
+        } else {
+          console.error("Camera.add used with wrong param name (angle|pos)")
+        }
+      } else {
+        console.error("Camera.add used with wrong params")
+      }
+    }
+
   }
 
   /**
@@ -70,53 +148,96 @@ const Utils3 = (function () {
    * Composer class
    */
   u.Composer = class {
-    constructor ({renderer, scene, camera} = {}) {
+    constructor ({
+      renderer,
+      scene,
+      camera,
+      bloom = false,
+      blur = false,
+      film = false,
+      bleach = false,
+      dotScreen = false,
+      vignette = false
+    } = {}) {
       this._composer = () => console.info("Not init with this.updatePass()")
       
       this._renderer = renderer.get()
       this._scene = scene
       this._camera = camera.get()
       
+      this._config = {
+        bloom: bloom,
+        blur: blur,
+        film: film,
+        bleach: bleach,
+        dotScreen: dotScreen,
+        vignette: vignette
+      }
+      
+      // Basic render
       this._renderPass = new THREE.RenderPass(this._scene, this._camera)
 
-      this._filmPass = new THREE.FilmPass(1, 0.01, 1000, false)
-      this._filmPass.uniforms["time"].value = 10
-      console.log(this._filmPass.uniforms)
+      // TV effect
+      this._filmPass = new THREE.FilmPass(0.5, 0.05, 1000, false)
+      // Bloom effect
+      this._bloomPass = new THREE.BloomBlendPass(8, 1, new THREE.Vector2(1024, 1024))
 
-      this._bloomPass = new THREE.BloomPass(0.5)
-
+      // Bleach effect
       this._bleachPass = new THREE.ShaderPass(THREE.BleachBypassShader)
-      this._bleachPass.uniforms["opacity"].value = 0.9
       
+      // Vignette effect
       this._vignettePass = new THREE.ShaderPass(THREE.VignetteShader)
       this._vignettePass.uniforms["offset"].value = 0.8
       this._vignettePass.uniforms["darkness"].value = 2
 
+      // Blur effect
       this._hBlurPass = new THREE.ShaderPass(THREE.HorizontalBlurShader)
       this._vBlurPass = new THREE.ShaderPass(THREE.VerticalBlurShader)
       this._hBlurPass.uniforms['h'].value = 1 / 700
       this._vBlurPass.uniforms['v'].value = 1 / 700
 
+      // Dot screen effect
       this._dotScreenPass = new THREE.DotScreenPass(new THREE.Vector2(0, 0), 0.5, 0.8)
       //this._dotScreenPass.uniforms['scale'].value = 2
 
+      // CopyPass
       this._copyPass = new THREE.ShaderPass(THREE.CopyShader)
+      this._copyPass.renderToScreen = true
+
       this.updatePass()
     }
     
     // Update pass according to options
-    updatePass () {
+    updatePass ({
+      bloom,
+      blur,
+      film,
+      bleach,
+      dotScreen,
+      vignette
+    } = {}) {
+      if (bloom != undefined) { this._config.bloom = bloom}
+      if (blur != undefined) { this._config.blur = blur}
+      if (film != undefined) { this._config.film = film}
+      if (bleach != undefined) { this._config.bleach = bleach}
+      if (dotScreen != undefined) { this._config.dotScreen = dotScreen}
+      if (vignette != undefined) { this._config.vignette = vignette}
+
       this._composer = new THREE.EffectComposer(this._renderer)
       this._composer.addPass(this._renderPass)
-      //this._composer.addPass(this._hBlurPass)
-      //this._composer.addPass(this._vBlurPass)
-      //this._composer.addPass(this._bloomPass)
-      this._composer.addPass(this._filmPass)
-      this._composer.addPass(this._bleachPass)
-      //this._composer.addPass(this._dotScreenPass)
-      this._composer.addPass(this._vignettePass)
+
+      if (this._config.blur) {
+        this._composer.addPass(this._hBlurPass)
+        this._composer.addPass(this._vBlurPass)
+      }
+
+      if (this._config.bloom) { this._composer.addPass(this._bloomPass) }
+      if (this._config.film) { this._composer.addPass(this._filmPass) }
+      if (this._config.bleach) { this._composer.addPass(this._bleachPass) }
+      if (this._config.dotScreen) { this._composer.addPass(this._dotScreenPass) }
+      if (this._config.vignette) { this._composer.addPass(this._vignettePass) }
+
       this._composer.addPass(this._copyPass)
-      this._copyPass.renderToScreen = true
     }
 
     updateSize (width, height) {
